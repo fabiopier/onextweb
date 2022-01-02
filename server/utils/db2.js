8ad49735a9bde24c83904f367b0b1aed926f5caa
fabiopier <fabiopier@hotmail.it>
2022-01-02 09:56:33 +0100

    fix puntamenti database

--- server/utils/db2.js
+++ server/utils/db2.js
@@ -1,11 +1,27 @@
 
 const mysql = require('mysql2');
 
+
+console.log('connessione stabilità?');
+console.log(process.env.NODE_DATABASE);
+console.log(process.env.NODE_DATABASE_USER);
+console.log(process.env.NODE_DATABASE_PW);
+console.log(process.env.NODE_DATABASE_URL);
+/*
 const pool = mysql.createPool({
     host: 'localhost',
     database: 'corso_nodejs',
     user: 'root',
     password: ''
+});*/
+
+
+
+const pool = mysql.createPool({
+    host:process.env.NODE_DATABASE_URL,
+    database: process.env.NODE_DATABASE,
+    user: process.env.NODE_DATABASE_USER,
+    password: process.env.NODE_DATABASE_PW
 });
 
 module.exports = pool.promise();
