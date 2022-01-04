2764656f7bcad5d72c0a61136804b673294ff6cc
fabiopier <fabiopier@hotmail.it>
2022-01-04 16:54:39 +0100

    added breakpoints

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../services/books.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,30 +10,54 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent {
- 
-  constructor(private booksService: BooksService) { }
 
+export class HomeComponent implements OnInit {
+  headerColumn: Number = 1;
+  breakpointObserver_ : Subscription;
   books = [];
-  
-  ngOnInit() {
-    this.getBooks();   
- }
 
- getBooks(){
-   this.booksService.getBooks()
-   .subscribe(
-     (data: any) => {
-       console.log('ritorno dei books');
-       console.log(JSON.stringify(data));
-       this.books = data.data;
-       
-   
-     },
-     (err: HttpErrorResponse) => {console.log(err)});
- }
-  
+  constructor(private booksService: BooksService, breakpointObserver: BreakpointObserver) {
+   this.breakpointObserver_=  breakpointObserver.observe(['(max-width: 1279.98px)']).subscribe(
+      (result: BreakpointState) => {
+        if (result.matches) {
+          console.log('match');
+          this.headerColumn = 2;
+        } else {
+          this.headerColumn = 1;
+        }
+      }
+    )
+  }
+
+  ngOnInit() {
+    this.configLayout();
+    this.getBooks();
+  }
+
+  configLayout() {
+
+  }
+
+  getBooks() {
+    this.booksService.getBooks()
+      .subscribe(
+        (data: any) => {
+          console.log('ritorno dei books');
+          console.log(JSON.stringify(data));
+          this.books = data.data;
+
+
+        },
+        (err: HttpErrorResponse) => { console.log(err) });
+  }
+
   callApi() {
-   console.log('welcome');
-}
+    console.log('welcome');
+  }
+
+  ngOnDestroy() {
+    this.breakpointObserver_.unsubscribe
+    console.log('destroy');
+  }
+
 }
