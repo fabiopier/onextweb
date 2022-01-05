e01bfd64c3490f0fb1e9528d372271907a215a7c
fabiopier <fabiopier@hotmail.it>
2022-01-05 10:22:07 +0100

    add grid system

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,9 @@ import { BooksService } from '../../services/books.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Subscription } from 'rxjs';
-
+import {
+  MatGridListModule
+} from '@angular/material/grid-list';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,8 +14,14 @@ import { Subscription } from 'rxjs';
 
 
 export class HomeComponent implements OnInit {
-  headerColumn: Number = 1;
-  breakpointObserver_! : Subscription;
+
+  breakpointObserverLarge!: Subscription;
+  breakpointObserverMedium! : Subscription;
+  breakpointObserverSmall! : Subscription;
+  
+
+
+  servicesCols: Number = 1;
   books = [];
 
   constructor(private booksService: BooksService, private breakpointObserver: BreakpointObserver) {
@@ -47,33 +55,36 @@ export class HomeComponent implements OnInit {
 
 
   breakpointObserverLayout() {
-    /*
-    XSmall	(max-width: 599.98px)
-    Small	(min-width: 600px) and (max-width: 959.98px)
-    Medium	(min-width: 960px) and (max-width: 1279.98px)
-    Large	(min-width: 1280px) and (max-width: 1919.98px)
-    XLarge	(min-width: 1920px)
-    Handset	(max-width: 599.98px) and (orientation: portrait), (max-width: 959.98px) and (orientation: landscape)
-    Tablet	(min-width: 600px) and (max-width: 839.98px) and (orientation: portrait), (min-width: 960px) and (max-width: 1279.98px) and (orientation: landscape)
-    Web	(min-width: 840px) and (orientation: portrait), (min-width: 1280px) and (orientation: landscape)
-    HandsetPortrait	(max-width: 599.98px) and (orientation: portrait)
-    TabletPortrait	(min-width: 600px) and (max-width: 839.98px) and (orientation: portrait)
-    WebPortrait	(min-width: 840px) and (orientation: portrait)
-    HandsetLandscape	(max-width: 959.98px) and (orientation: landscape)
-    TabletLandscape	(min-width: 960px) and (max-width: 1279.98px) and (orientation: landscape)
-    WebLandscape	(min-width: 1280px) and (orientation: landscape)
-  */ 
 
-    this.breakpointObserver_=  this.breakpointObserver.observe([Breakpoints.Web]).subscribe(
+    //Breakpoints.Web
+    this.breakpointObserverLarge=  this.breakpointObserver.observe(['(min-width: 960px)']).subscribe(
       (result: BreakpointState) => {
         if (result.matches) {
           console.log('match');
-          this.headerColumn = 2;
-        } else {
-          this.headerColumn = 1;
-        }
+          this.servicesCols = 2;
+        } 
       }
     )
+
+    this.breakpointObserverMedium=  this.breakpointObserver.observe(['(max-width: 959.98px)']).subscribe(
+      (result: BreakpointState) => {
+        if (result.matches) {
+          console.log('match');
+          this.servicesCols = 1;
+        } 
+      }
+    )
+
+    this.breakpointObserverSmall =  this.breakpointObserver.observe(['(max-width: 600px)']).subscribe(
+      (result: BreakpointState) => {
+        if (result.matches) {
+          console.log('match');
+          this.servicesCols = 1;
+        } 
+      }
+    )
+
+
   }
 
 
@@ -81,7 +92,10 @@ export class HomeComponent implements OnInit {
 
 
   ngOnDestroy() {
-    this.breakpointObserver_.unsubscribe
+    this.breakpointObserverLarge.unsubscribe();
+    this.breakpointObserverMedium.unsubscribe();
+    this.breakpointObserverSmall.unsubscribe();
+    
     console.log('destroy');
   }
 
